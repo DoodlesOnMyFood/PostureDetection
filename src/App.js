6ff8f357b1b212c985fbb6295dbd0b453798a3c2
jiggyboo <tkdduqcom@gmail.com>
2020-10-30 16:36:52 +0900

    minor color change

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return(
     <div >
       <header className="App-header">
-        <h2>교정교관</h2>
+        <h2>PoseSergeant</h2>
       </header>
       <div className="App-body">
         <AppInner />
