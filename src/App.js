7ce9a0f513d12355bcebda157b7422a245d0b402
jiggyboo <tkdduqcom@gmail.com>
2020-10-30 22:21:50 +0900

    button fiddle

--- src/App.js
+++ src/App.js
@@ -11,15 +11,15 @@ function App() {
       <div className="App-body">
         <AppInner />
       </div>
-      <div className="w3-row-padding w3-center w3-theme">
+      <div className="w3-row-padding w3-center w3-theme" style={{minHeight:"20vh", alignItems:"center"}}>
         <div className="w3-third">
-          <button className="w3-card w3-container w3-round-medium" style={{minHeight:"17vh",backgroundColor:"#282c34"}} onClick={()=>1+1}>Button1</button>
+          <button className="w3-card w3-container w3-round-medium" style={{minWidth:"50vh",minHeight:"17vh",color:"#fff",backgroundColor:"#282c34"}} onClick={()=>1+1}>Button1</button>
         </div>
         <div className="w3-third">
-          <button className="w3-card w3-container w3-round-medium" style={{minHeight:"17vh",backgroundColor:"#282c34"}} onClick={()=>1+1}>Button2</button>
+          <button className="w3-card w3-container w3-round-medium" style={{minWidth:"50vh",minHeight:"17vh",color:"#fff",backgroundColor:"#282c34"}} onClick={()=>1+1}>Button2</button>
         </div>
         <div className="w3-third">
-          <button className="w3-card w3-container w3-round-medium" style={{minHeight:"17vh",backgroundColor:"#282c34"}} onClick={()=>1+1}>Button3</button>
+          <button className="w3-card w3-container w3-round-medium" style={{minWidth:"50vh",minHeight:"17vh",color:"#fff",backgroundColor:"#282c34"}} onClick={()=>1+1}>Button3</button>
         </div>        
       </div>
     </div> 
