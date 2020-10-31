1db4ed825f2fc7c3e6a882f61dd23bdc2937de67
jiggyboo <tkdduqcom@gmail.com>
2020-10-31 16:20:50 +0900

    rid of unused

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
 import AppInner from "./AppInner";
 import Container from 'react-bootstrap/Container'
-import ButtonGroup from 'react-bootstrap/ButtonGroup'
-import Button from 'react-bootstrap/Button'
+
 
 function App() {
   return(
