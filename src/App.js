47ef17d202a70a0d2e7d9545fe3afd0fe4523300
jiggyboo <tkdduqcom@gmail.com>
2020-10-31 16:38:15 +0900

    switch to boot

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React from "react";
 import AppInner from "./AppInner";
 import Container from 'react-bootstrap/Container'
+import Row from '/react-bootstrap/Row'
+import Col from '/react-bootstrap/Col'
 
 
 function App() {
   return(
     <Container fluid className="p-3">
-      <header className="App-header">
-        <h2>PoseSergeant</h2>
-      </header>
-      <div className="App-body">
-        <AppInner />
-      </div>
-
+      <Row>
+        <Col>교정교관</Col>
+      </Row>
+      <Row>
+        <Col>
+          <AppInner />
+        </Col>
+      </Row>
+      <Row>
+        <Col></Col>
+      </Row>
     </Container> 
   )
 }
