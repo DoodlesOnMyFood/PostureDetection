5a5d0f17cb03c274ec5c5eeef7b31c7d7395d72e
jiggyboo <tkdduqcom@gmail.com>
2020-11-01 01:48:04 +0900

    image imported

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Image} from 'react-native'
 import Container from 'react-bootstrap/Container'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
