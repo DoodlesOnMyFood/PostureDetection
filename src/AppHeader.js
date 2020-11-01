f8457d824e43cf8362d6d58f4efb179d8d4cdb27
jiggyboo <tkdduqcom@gmail.com>
2020-11-01 11:58:21 +0900

    setting dropdown(trying to change menu alignment)

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -29,8 +29,8 @@ function App_Header() {
               <Nav>
                 <NavDropdown 
                   title={<Image src={require('./Images/cog(gray).png')} style={ImageIconStyle}/>} 
-                  id="dropdown-menu-align-right"
-                  menuAlign='right'>
+                  id="collasible-nav-dropdown"
+                  >
                     <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                     <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                     <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
