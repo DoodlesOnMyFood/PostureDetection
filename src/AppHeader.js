23657561b295bdd7f29aa0cb1a96f84d71fbc5f7
jiggyboo <tkdduqcom@gmail.com>
2020-11-01 11:42:50 +0900

    dropdown

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -29,13 +29,14 @@ function App_Header() {
               <Nav>
                 <NavDropdown 
                   title={<Image src={require('./Images/cog(gray).png')} style={ImageIconStyle}/>} 
-                  menuAlign="right"
                   id="collasible-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                    <NavDropdown.Menu align='right'>
+                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                    </NavDropdown.Menu>
                 </NavDropdown>
               </Nav>
             </Navbar.Collapse>
