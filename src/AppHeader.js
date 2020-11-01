bd37219c22edbb0f83a5dd33c3efe1ef7526f504
jiggyboo <tkdduqcom@gmail.com>
2020-11-01 11:28:14 +0900

    setting dropdown

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -27,8 +27,12 @@ function App_Header() {
                 </NavDropdown>
               </Nav>
               <Nav>
-                <NavDropdown title={<Image src={require('./Images/cog(gray).png')} style={ImageIconStyle}/>} 
-                id="collasible-nav-dropdown">
+                <NavDropdown 
+                  title={<Image src={require('./Images/cog(gray).png')} style={ImageIconStyle}/>} 
+                  id="collasible-nav-dropdown"
+                  menuAlign="right"
+                  title="Dropdown right"
+                  id="dropdown-menu-align-right">
                     <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                     <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                     <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
