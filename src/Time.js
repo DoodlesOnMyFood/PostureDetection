0f73b2782247fbe9a53bbea1c05a68b38d3b3fbc
jiggyboo <tkdduqcom@gmail.com>
2020-11-04 22:52:38 +0900

    Digital Clock(not updating)

--- src/Time.js
+++ src/Time.js
@@ -7,14 +7,35 @@ export default () =>{
     const [running, setRunning] = useState(true)
     const tickTock = useRef(null)
     const timer = useRef(null)
+    const [hour, setHour] = useState(0)
+    const [min, setMin] = useState(0)
+    const [sec, setSec] = useState(0)
 
     useEffect(() =>{
         if(tickTock.current === null){
             timer.current = new Timer()
             timer.current.start()
+            var date = new Date();
+            setHour(date.getHours())
+            setMin(date.getMinutes())
+            setSec(date.getSeconds())
             tickTock.current = setInterval(() => {
                 if (Math.round(timer.current.getTime() / 1000) > time){
+                    setSec(sec+1)
+                    if (sec===60){
+                        setSec(0)
+                        setMin(min+1)
+                        if(min===60){
+                            setMin(0)
+                            setHour(hour+1)
+                            if(hour===24){
+                                setHour(0)
+                            }
+                        }
+                        return
+                    }
                     setTime(Math.round(timer.current.getTime() / 1000))
+
                 }
             } , 100)
         }
@@ -28,10 +49,12 @@ export default () =>{
     }, [])
 
     const timeClass = running ? "timerClass" : "" // maybe implement returning animations
-
+    
     return (
         <div className={timeClass}>
             {time}
+            <br></br>
+            {hour+":"+min+":"+sec}
         </div>
     )
 }
